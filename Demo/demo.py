380ec18ac7bd6815f85f69fffbadcad890528bfc
JungnamKim <kim1014j@naver.com>
2018-12-03 11:54:29 +0900

    update

--- Demo/demo.py
+++ Demo/demo.py
@@ -1,28 +1,17 @@
 from cims_db import dbmanager
 import copy
 
-dbmanager.Connect('10.201.5.101', '5432', 'cims', 'comtec', 'comtec')
+dbmanager.Connect('<HOST_IP>', '<HOST_PORT>', '<DB_NAME>', '<ID>', '<PW>')
 
-ip_info = None
+return_result = None
 try :
-	query = "SELECT IP_SEQ, "\
-			"        GROUP_SEQ, "\
-			"        IP_ADDR, "\
-			"        IP_DESC, "\
-			"        ip_assign_flag, "\
-			"        ip_assign_user_id, "\
-			"        ip_assign_time, "\
-			"        IP_ACTIVE_FLAG, "\
-			"        IP_ACTIVE_TIME, "\
-			"        IP_ACTIVE_CLOSE_TIME "\
-			"FROM TB_D_IP "\
-			"WHERE IP_SEQ = %s" % ('46')
+	query = "SELECT * FROM <TABLE>"
 	cursor = dbmanager.Select(query)
 	if cursor is not None:
 		result = cursor.fetchone()
-		ip_info = copy.deepcopy(result)
+		return_result = copy.deepcopy(result)
 		cursor.close()
 except Exception as e:
 	print e
 
-print ip_info
+print return_result
